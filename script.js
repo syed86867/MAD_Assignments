76a43f984e0938c7b911708c3f0d7f9d54ae2bfb
Syed Ashir Ali <ashir9706@gmail.com>
2025-03-01 12:54:53 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+//example of string methods
+
+let name="SyedAshirAli";
+console.log(name);
+console.log(name.length);
+console.log(name.toUpperCase());
+console.log(name.toLowerCase());
+console.log(name.charAt(0));
+console.log(name.charAt(1));
+console.log(name.charAt(2));
+console.log(name.indexOf('A'));
+console.log(name.lastIndexOf('i'));
+console.log(name.includes('Ashir'));
+console.log(name.startsWith('Syed'));
+console.log(name.endsWith('Ali'));
+console.log(name.substring(4, 9));
+console.log(name.slice(-3));
+console.log(name.split('r'));
+console.log(name.replace('Syed', 'Mr.'));
+
+//example of array methods
+let arr = ['Syed', 'Ashir', 'Ali', 'JavaScript', 'Developer',2025];
+console.log(arr);
+console.log(arr.length);
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr.indexOf('Ali'));
+console.log(arr.lastIndexOf('Developer'));
+console.log(arr.includes('Java'));
+console.log(arr.join(''));
+console.log(arr.slice(2, 5));
+console.log(arr.splice(1, 2));
+console.log(arr.push('Developer'));
+console.log(arr.pop());
+console.log(arr.shift());
+console.log(arr.unshift('Syed'));
+console.log(arr.reverse());
+
+let arr2=['comsats', 'University'];
+console.log(arr.concat(arr2));
+   
